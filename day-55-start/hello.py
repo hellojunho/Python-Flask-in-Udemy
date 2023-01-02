9e07000d4efe138db215618400aad86ba44db191
DESKTOP-IG540Q0\jo626 <jo62698476@gmail.com>
2023-01-02 17:17:30 +0900

    [update] hello.py

--- day-55-start/hello.py
+++ day-55-start/hello.py
@@ -7,6 +7,7 @@ def hello_world():
     return '<h1 style="text-align: center">Hello World</h1>' \
            '<p>This is a paragraph</p>' \
            '<img src="https://media1.giphy.com/media/MEdRTaYvZBlVMEjwZ3/giphy360p.mp4?cid=ecf05e47tucdy8ljyikyyzashiaom71jeye9pllitu8stukn&rid=giphy360p.mp4&ct=v", width=200>'
+
 @app.route('/bye')
 def bye():
     return "Bye"
