681416ceca1cce3ba344c359bb46ee8117fd90e6
junho <jo62698476@gmail.com>
2023-01-04 20:33:08 +0900

    [update] main.py

--- day-57-project-blog/main.py
+++ day-57-project-blog/main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/blog')
+def blog():
+    return render_template()
+
 if __name__ == "__main__":
     app.run(debug=True)
